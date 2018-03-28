b7f4f1b2eddb2b954c72e5102f2a0c49505d9a0f
Haoran Wang <vitowang1998@gmail.com>
2018-03-28 01:48:28 -0400

    Adds an important information for state in RL.

--- intro/basics.py
+++ intro/basics.py
@@ -37,23 +37,28 @@ class UnsupervisedLearning
     
 
 # RL, on the other hand, has a much bigger interface.
-# It takes the entire environment instead of several training parameters X and Y as the parameter.
+# It takes the entire environment instead of several training parameters X and
+#     Y as the parameter.
 
 # RL can be used in psychology and predicting animal behavior.
 
 # SL focuses on increase performance and lower the cost. 
-# Unlike SL and UL, RL focuses on getting a message back from the environment, and this process should be autometic.
-# In SL, labels need to be manually added so that the algorithm can be trained, but in RL, we train the algorithm based
-#   on the feedback from the environment. 
+# Unlike SL and UL, RL focuses on getting a message back from the environment, 
+#     and this process should be autometic.
+# In SL, labels need to be manually added so that the algorithm can be trained, 
+#     but in RL, we train the algorithm based on the feedback from the environment. 
 
 
 
 # Terminologies
-# 1. Agent: The object that lives in the environment. The object into which we are trying to code AI.
+# 1. Agent: The object that lives in the environment. The object into which we 
+#           are trying to code AI.
 # 2. Environment: The place, whether virtual or real, in which the agent lives.
-# 3. State: DIfferent combinations of the factor parameters of the environment.
-# 4. Rewards: The feedback from the environment. Positive reward should be given if AI performs expectedly, otherwise, negative reward
-#   should be given.
+# 3. State: DIfferent combinations of the factor parameters of the environment. 
+#           State only involves the information that the agent can sense.
+# 4. Rewards: The feedback from the environment. Positive reward should be given
+#             if AI performs expectedly, otherwise, negative reward
+#             should be given.
 # 5. SAR Triples: abbe. of State, Action, Reward.
 
 
